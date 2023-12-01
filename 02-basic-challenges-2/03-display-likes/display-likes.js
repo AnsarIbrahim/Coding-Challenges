05a50f6cebbf9ec8c2bf9468062e3f84a54c36b1
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-01 12:40:47 +0530

    Display Likes Challenges

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,17 @@
-function displayLikes() {}
+function displayLikes(arr) {
+  const output = arr.length;
+
+  if (output === 0) {
+    return 'no one likes this';
+  } else if (output === 1) {
+    return `${arr[0]} likes this`;
+  } else if (output === 2) {
+    return `${arr[0]} and ${arr[1]} like this`;
+  } else if (output === 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  } else {
+    return `${arr[0]}, ${arr[1]} and ${output - 2} others like this`;
+  }
+}
 
 module.exports = displayLikes;
