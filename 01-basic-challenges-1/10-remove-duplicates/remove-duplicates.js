95a0f27792f0aaacf494f81160d3d05e6c8b6d67
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-01 11:03:15 +0530

    Remove Duplicates Challenge

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,13 +1,15 @@
 function removeDuplicates(arr) {
-  const findArr = [];
+  //   const findArr = [];
 
-  for (i = 0; i < arr.length; i++) {
-    if (!findArr.includes(arr[i])) {
-      findArr.push(arr[i]);
-    }
-  }
+  //   for (i = 0; i < arr.length; i++) {
+  //     if (!findArr.includes(arr[i])) {
+  //       findArr.push(arr[i]);
+  //     }
+  //   }
 
-  return findArr;
+  //   return findArr;
+
+  return Array.from(new Set(arr));
 }
 
 module.exports = removeDuplicates;
