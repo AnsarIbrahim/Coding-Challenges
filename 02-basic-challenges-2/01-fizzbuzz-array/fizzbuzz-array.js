fe0d4f2e12fc38051776c9e62fce9375186c5afb
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-01 11:27:04 +0530

    FizzBuzz Challenge

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(arr) {
+  const newArr = [];
+
+  for (let i = 1; i <= arr; i++) {
+    newArr.push(
+      i % 15 === 0
+        ? 'FizzBuzz'
+        : i % 3 === 0
+        ? 'Fizz'
+        : i % 5 === 0
+        ? 'Buzz'
+        : i
+    );
+  }
+
+  return newArr;
+}
 
 module.exports = fizzBuzzArray;
