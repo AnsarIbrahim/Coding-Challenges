3c2aad12344bd3b0492be4f1cbdd8afaac26f7a2
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-01 14:01:15 +0530

    Find Missing Number  Challenges

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,22 @@
-function findMissingNumber() {}
+function findMissingNumber(num) {
+  //   const sortedNum = num.sort((a, b) => a - b);
+  //   const firstNum = sortedNum[0];
+  //   const lastNum = sortedNum[sortedNum.length - 1];
+  //   const total = ((lastNum - firstNum + 1) * (firstNum + lastNum)) / 2;
+  //   const sum = sortedNum.reduce((acc, curr) => acc + curr, 0);
+  //   return total - sum;
+
+  if (num.length === 0) {
+    return undefined;
+  }
+  const n = num.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  //   const actualSum = num.reduce((sum, num) => sum + num, 0);
+  let actualSum = 0;
+  for (let i = 0; i < num.length; i++) {
+    actualSum += num[i];
+  }
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
