00867e14af80bf802f90ba7658ce67675a958f71
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-01 10:10:44 +0530

    Vowels Challenge

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,15 @@
-function countVowels() {}
+function countVowels(str) {
+  const formattedStr = str.toLowerCase();
+  const vowels = 'aeiou';
+  let result = 0;
+
+  for (i = 0; i < formattedStr.length; i++) {
+    if (vowels.includes(formattedStr[i])) {
+      result++;
+    }
+  }
+
+  return result;
+}
 
 module.exports = countVowels;
