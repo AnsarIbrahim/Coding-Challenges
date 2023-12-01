60f3c22fa2e01a1f1cce821c42f7fd16a0a4c6db
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-01 12:18:24 +0530

    InterSection Challenges

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,22 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  //   const interSectionArr = [];
+
+  //   for (i = 0; i < arr1.length; i++) {
+  //     if (arr2.includes(arr1[i]) && !interSectionArr.includes(arr1[i])) {
+  //       interSectionArr.push(arr1[i]);
+  //     }
+  //   }
+
+  const set1 = new Set(arr1);
+  const interSectionArr = [];
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      interSectionArr.push(num);
+    }
+  }
+
+  return interSectionArr;
+}
 
 module.exports = arrayIntersection;
