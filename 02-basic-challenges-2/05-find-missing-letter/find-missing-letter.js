fbed80bd879803cfd1a84d2a2385ba3be0c0ed2b
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-02 20:04:22 +0530

    Find Missing letters Challenges

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,12 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const start = alphabet.indexOf(arr[0]);
+  const end = alphabet.indexOf(arr[arr.length - 1]);
+  const subAlphabet = alphabet.substring(start, end + 1);
+  const missingLetter = subAlphabet
+    .split('')
+    .filter((letter) => !arr.includes(letter))[0];
+  return arr[0] === arr[0] ? missingLetter : missingLetter;
+}
 
 module.exports = findMissingLetter;
