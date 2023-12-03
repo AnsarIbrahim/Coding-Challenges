a865c9848e567148452a637e362d2d9e023aa18c
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-03 13:16:42 +0530

    Update find-missing-letter.js

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,12 +1,30 @@
 function findMissingLetter(arr) {
+  //const alphabet= 
+//'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ // const start = alphabet.indexOf(arr[0]);
+  //const end = alphabet.indexOf(arr[arr.length - 1]);
+//  const subAlphabet = alphabet.substring(start, end + 1);
+  //const missingLetter = subAlphabet
+    //.split('')
+    //.filter((letter) => !arr.includes(letter))[0];
+  //return arr[0] === arr[0] ? missingLetter : missingLetter;
+
+
+
   const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const start = alphabet.indexOf(arr[0]);
-  const end = alphabet.indexOf(arr[arr.length - 1]);
-  const subAlphabet = alphabet.substring(start, end + 1);
-  const missingLetter = subAlphabet
-    .split('')
-    .filter((letter) => !arr.includes(letter))[0];
-  return arr[0] === arr[0] ? missingLetter : missingLetter;
+  // Find the index of the first letter in the array in the alphabet string
+  const startIndex = alphabet.indexOf(arr[0]);
+
+  // Loop through the array
+  for (let i = 0; i < arr.length; i++) {
+    // If the current letter in the array is not the same as the current letter in the alphabet string, return the current letter in the alphabet string
+    if (arr[i] !== alphabet[startIndex + i]) {
+      return alphabet[startIndex + i];
+    }
+  }
+
+  // If no letter is missing, return an empty string
+  return '';
 }
 
 module.exports = findMissingLetter;
